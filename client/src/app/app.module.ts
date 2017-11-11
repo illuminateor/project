7c3b5b1e285dc9990a2db15cf06bc849eb19dd74
Oleh Mudrak <o.mudrak93@gmail.com>
2017-11-11 19:08:45 +0100

    added navbar, home and dashboard components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
 import {AppRoutingModule} from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
