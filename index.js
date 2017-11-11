a86fda846d0af2a1369abd4ef95f0fe2883491ba
Oleh Mudrak <o.mudrak93@gmail.com>
2017-11-11 16:39:45 +0100

    server setup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const path = require('path');
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, { useMongoClient: true },(err)=>{
@@ -12,8 +14,10 @@ mongoose.connect(config.uri, { useMongoClient: true },(err)=>{
         console.log('Connected to database: ' + config.db)
 }});
 
+app.use(express.static(__dirname + '/client/dist/'));
+
 app.get('*', (req, res) => {
-    res.send('hellos world');
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'))
 });
 
 app.listen(3000, () => {
